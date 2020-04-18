1f280639f45bd183395d5e30d9743c88dade1c9f
crssnky <vindicare28@gmail.com>
2020-04-18 15:29:06 +0900

    fix querying if it have japanese.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ extern crate rocket;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate percent_encoding;
 extern crate serde_json;
 extern crate ureq;
 extern crate url;
+use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
 use url::form_urlencoded;
 
 #[derive(Debug, Deserialize)]
@@ -45,17 +47,20 @@ struct Response {
 
 #[get("/<subject>")]
 fn get_data(subject: String) -> String {
-  let quety = format!("PREFIX schema: <http://schema.org/>PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/RDFs/detail/>SELECT * WHERE {{ imas:{} ?n ?o;}}order by (?n)", subject);
-  let encoded_subject = form_urlencoded::Serializer::new(String::new())
+  const FRAGMENT: &AsciiSet = &CONTROLS;
+  let encoded_subject = utf8_percent_encode(&subject, FRAGMENT).to_string();
+  let quety = format!("PREFIX schema: <http://schema.org/>PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/RDFs/detail/>SELECT * WHERE {{ imas:{} ?n ?o;}}order by (?n)", encoded_subject);
+  let encoded_query = form_urlencoded::Serializer::new(String::new())
     .append_pair("output", "json")
     .append_pair("force-accept", "text/plain")
     .append_pair("query", &quety)
     .finish();
   let base_url = format!(
     "https://sparql.crssnky.xyz/spql/imas/query?{}",
-    encoded_subject
+    encoded_query
   );
   let res = ureq::get(&base_url).call();
+  println!("{}", base_url);
 
   if res.ok() {
     let json_str = res.into_string().unwrap();
